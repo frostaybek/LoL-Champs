433a7cfa758ca82336512ddf4ab1a4f2deb79aee
Ayaz Aybek <87331452+frostaybek@users.noreply.github.com>
2023-10-27 16:24:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var heading = document.getElementById("myHeading");
+heading.addEventListener("click", function() {
+heading.style.backgroundColor = "#17ba5e";
+});
+
+function fun(){
+    var heading = document.getElementById("myHeading");
+    var content = heading.innerHTML = "You right! Im jungler and mid 😀";
+}
